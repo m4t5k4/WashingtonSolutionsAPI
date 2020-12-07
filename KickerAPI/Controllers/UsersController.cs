59ee04b4853c26d262822a0d12ba9736dd5f5550
Seppe Alaerts <r0749143@student.thomasmore.be>
2020-12-07 11:04:42 +0100

    UserController

--- KickerAPI/Controllers/UsersController.cs
+++ KickerAPI/Controllers/UsersController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using KickerAPI.Data;
 using KickerAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using KickerAPI.Services;
 
 namespace KickerAPI.Controllers
 {
@@ -14,6 +16,7 @@ namespace KickerAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private IUserService _userService;
         private readonly KickerContext _context;
 
         public UsersController(KickerContext context)
@@ -21,72 +24,34 @@ namespace KickerAPI.Controllers
             _context = context;
         }
 
-        // GET: api/Users
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
+            var username = User.Claims.FirstOrDefault(c => c.Type == "Username").Value;
             return await _context.Users.ToListAsync();
         }
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-        // PUT: api/Users/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
-        {
-            if (id != user.UserID)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Users
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUser", new { id = user.UserID }, user);
+            return Ok(user);
         }
 
-        // DELETE: api/Users/5
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
         [HttpDelete("{id}")]
         public async Task<ActionResult<User>> DeleteUser(int id)
         {
