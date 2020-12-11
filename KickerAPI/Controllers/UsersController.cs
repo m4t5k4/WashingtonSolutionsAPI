116db6bbca8cdd1ea4c39565f9eecf746266aa3f
Seppe Alaerts <r0749143@student.thomasmore.be>
2020-12-11 15:50:23 +0100

    update user

--- KickerAPI/Controllers/UsersController.cs
+++ KickerAPI/Controllers/UsersController.cs
@@ -72,6 +72,37 @@ namespace KickerAPI.Controllers
             return Ok(user);
         }
 
+        // PUT: api/Users/5
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
         // DELETE: api/Users/{id}
         [HttpDelete("{id}")]
         public async Task<ActionResult<User>> DeleteUser(int id)
